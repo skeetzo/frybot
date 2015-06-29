1f8b2e6de566b5b36db1eb9ec4ed8340a6202ee6
Schizo <alexjoberg@gmail.com>
2015-06-28 23:04:54 -0700

    Version 0.0.0 6/28/2015 ...

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ function scytalia() {
   var commandsRegex = "([//]{1}"+commands.join("|")+")?("+comandsArguments.join("|")+")?";
   commandsRegex = new RegExp(commandsRegex, "gi");
 
-  scytalia.respond = function() {
+  var respond = function() {
     startThinking();
     botResponse = 'nuh uh';
     return;
