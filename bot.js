155f51e05b26eef9a861ac6d77ac65a1e92a6adc
Schizo <alexjoberg@gmail.com>
2015-07-01 00:47:46 -0700

    Version 0.0.0 6/28/2015 ...

--- bot.js
+++ bot.js
@@ -114,7 +114,7 @@ function messageCheck(message) {
   var argument = message.match(commandsRegex)[2];
   console.log('Command:'+command);
   console.log('Argument:'+argument);
-  bot[command](argument,message);
+  this[command](argument,message);
   botResponse = message;
   startThinking();
 }
