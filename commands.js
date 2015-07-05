50599700c4ca29a39671844f276504da095b1e0a
Schizo <alexjoberg@gmail.com>
2015-07-05 06:19:44 -0700

    V0.0.2 commands.js introduced commands structure solidifed

--- commands.js
+++ commands.js
@@ -29,7 +29,7 @@ function activate(theCommand, theSender) {
   var command = theCommand.match(commandsRegex)[0];
   var argument = theCommand.match(commandsRegex)[2];
 
-  var message = theCommand.substring(theCommand.lastIndexOf(commandsRegex));
+  var message = theCommand.match(commandsRegex)[3];
 
   if (theSender) {
     var i = theSender.indexOf(' ');
