abfcf50add82e00c47a7a6e062ba6065ec37e58a
Schizo <alexjoberg@gmail.com>
2015-08-31 22:19:00 -0700

    v0.0.3 bottle added groupme added reminder/cron added cron testing

--- index.js
+++ index.js
@@ -33,8 +33,7 @@ function ping() {
 
 var CronJob = require('cron').CronJob;
 var job = new CronJob({
-//  cronTime: '00 30 19 * * 1',
-  cronTime: '00 10 13 * * *',
+  cronTime: '00 30 19 * * 1',
   onTick: function() {
     /*
      * Runs every Monday
