d071a3d4995c9ff17f76f05d187d0272f0e5f779
brainlessdg13 <brainlessdg13@users.noreply.github.com>
2015-09-22 19:41:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -31,17 +31,17 @@ function ping() {
   this.res.end("Hi, I'm "+process.env.NAME+" and I totally work.");
 }
 
-var CronJob = require('cron').CronJob;
-var job = new CronJob({
- cronTime: '00 30 19 * * 1',
-  onTick: function() {
-    /*
-     * Runs every Monday
-     * at 7:30:00 PM.
-     */
-     bot.bottleReminder();
-  },
-  start: true,
-  timeZone: 'America/Los_Angeles'
-});
+// var CronJob = require('cron').CronJob;
+// var job = new CronJob({
+// cronTime: '00 30 19 * * 1',
+//   onTick: function() {
+//     /*
+//     * Runs every Monday
+//     * at 7:30:00 PM.
+//     */
+//     bot.bottleReminder();
+//   },
+//   start: true,
+//   timeZone: 'America/Los_Angeles'
+// });
 //job.start();
