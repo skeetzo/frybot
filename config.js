5b038a37c71f7e6a2d9f19c3750532a3046c6c83
Schizo <alexjoberg@gmail.com>
2015-10-08 18:09:49 -0700

    v0.0.4 almost complete fiddling

---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {};
+
+config.port = Number(process.env.PORT || 3000);
+config.name = "Frybot";
+
+config.GroupMe_AccessToken = "a6ac6c8049830133c62e61acffbaadbb";
+config.ItIsWhatItIs_GroupMeID = "7054026";
+config.botID = "219f82d5b599637927f208bb61";
+
+config.ItIsWhatItIs_serviceEmail = "615638101068-ddthvbjttd2076flaqi1rm54divhpqvk@developer.gserviceaccount.com";
+
+
+module.exports = config;
