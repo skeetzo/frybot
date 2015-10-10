8232fd05a4684949185ef891c1ae7115d1f0169b
Schizo <alexjoberg@gmail.com>
2015-10-10 09:38:16 -0700

    v0.0.4 moar fiddling

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function ping() {
 
 var CronJob = require('cron').CronJob;
 var job = new CronJob({
- cronTime: '00 30 19 * * 1',
+ cronTime: '00 40 09 * * 6',
   onTick: function() {
      bot.bottleReminder();
      setTimeout(bot.readyChecker(),10000);
