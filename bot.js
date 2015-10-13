384569bd14fc8b6f2657cef92d42c5f0628e1c6e
Schizo <alexjoberg@gmail.com>
2015-10-13 09:59:57 -0700

    v0.0.4 complete fiddling

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ function respond() {
     return;
   }
   var request = JSON.parse(this.req.chunks[0]);
-  if (request.name == 'Scytalia')
+  if (request.name == config.name && !config.talkingToMyself)
     return;
   if (request.text && request.name && commands.matches(request.text)) {
     if (request.name)
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.port = Number(process.env.PORT || 3000);
-config.name = "Frybot";
+config.name = "FryBot";
 
 config.GroupMe_AccessToken = "a6ac6c8049830133c62e61acffbaadbb";
 config.ItIsWhatItIs_GroupMeID = "7054026";
@@ -9,6 +9,7 @@ config.botID = "219f82d5b599637927f208bb61";
 
 config.minimumReadyPlayers = 5;
 config.readyTimerDelay = 6000000;
+config.talkingToMyself = false;
 
 config.ItIsWhatItIs_serviceEmail = "615638101068-ddthvbjttd2076flaqi1rm54divhpqvk@developer.gserviceaccount.com";
 
