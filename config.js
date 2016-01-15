17c946c0c8641771b295b24e96ece9fd5ee745dd
Schizo <alexjoberg@gmail.com>
2016-01-14 23:46:41 -0800

    uh

--- config.js
+++ config.js
@@ -24,17 +24,15 @@ var arguments = [
 ];
 var commandsRegex = "(\/"+commands.join("|\/")+")?("+arguments.join("|")+")?";
 config.commandsRegex = new RegExp(commandsRegex, "gi");
-
-// ItIsWhatItIs
-config.ItIsWhatItIs_Spreadsheet_ID = process.env.ItIsWhatItIs_Spreadsheet_ID;
-config.ItIsWhatItIs_statsSheetID = 'ot3ufy3';
-config.ItIsWhatItIs_frybotSheetID = 'om5ojbr';
 // Google
 config.Frybot_Google_ServiceEmail = process.env.Frybot_Google_ServiceEmail;
 config.Frybot_Google_key = process.env.Frybot_Google_key;
+// It Is What It Is Google Sheet
+config.ItIsWhatItIs_Spreadsheet_ID = process.env.ItIsWhatItIs_Spreadsheet_ID;
+config.ItIsWhatItIs_statsSheetID = 'ot3ufy3';
+config.ItIsWhatItIs_frybotSheetID = 'om5ojbr';
 // GroupMe API
 config.GroupMe_AccessToken = process.env.GroupMe_AccessToken;
-
 // Misc
 config.port = 3000;
 config.responding = true;
@@ -42,16 +40,16 @@ config.responseTime = 6000;
 config.brainfart = 10000;
 
 // Debugging sets personality switch
-config.debugging = true;
+config.debugging = false;
 if (!config.debugging) {
   config.name = "Frybot";
   config.GroupMe_group_ID = process.env.Frybot_GroupMe_Group_ID;
-  config.botID = process.env.Frybot_GroupMe_bot_ID
+  config.botID = process.env.Frybot_GroupMe_bot_ID;
 }
 else {
   config.name = "Naivi";
   config.GroupMeID = process.env.Naivi_GroupMe_Group_ID;
-  config.botID = process.env.Naivi_GroupMe_bot_ID
+  config.botID = process.env.Naivi_GroupMe_bot_ID;
 }
 
 module.exports = config;
