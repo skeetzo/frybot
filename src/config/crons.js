b544fe4e71ada64f30b5dd977b41879bcb029ee0
brainlessdg13 <brainlessdg13@gmail.com>
2017-01-14 12:24:14 -0800

    - to args

--- src/config/crons.js
+++ src/config/crons.js
@@ -5,7 +5,7 @@ module.exports = function() {
 		afterparty : {
 			start : true,
 			command : 'season',
-			argument : '-afterparty',
+			argument : 'afterparty',
 			cronTime : '00 00 12 * 0-5 3',
 	        timeZone: 'America/Los_Angeles'	
 		},
@@ -27,9 +27,9 @@ module.exports = function() {
 		pregame : {
 			start : true,
 			command : 'season',
-			argument : '-pregame',
+			argument : 'pregame',
 			cronTime : '00 00 10 * 0-5 2', 
-			cronTime : '00 16 12 * * *', 
+			cronTime : '00 26 12 * * *', 
 	        timeZone: 'America/Los_Angeles'	
 		}
 	}
