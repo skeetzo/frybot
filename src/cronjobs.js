fa584648bbd40990f85437347bb99dc12caa8e2a
brainlessdg13 <brainlessdg13@gmail.com>
2017-01-14 12:31:35 -0800

    - to args

--- src/cronjobs.js
+++ src/cronjobs.js
@@ -18,7 +18,7 @@ module.exports.start = function() {
       var cro = self.config.crons[key];
       var cron = new CronJob({
           cronTime: cro.cronTime,
-          onTick: function() {if (self.commands[cro.command] && typeof self.commands[cro.command] == 'function') self.commands[cro.command].call(self,cro),
+          onTick: function() {if (self.commands[cro.command] && typeof self.commands[cro.command] == 'function') self.commands[cro.command].call(self,cro)},
           start: cro.start,
           timeZone: cro.timeZone
       });
