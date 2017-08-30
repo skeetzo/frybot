953230489e9526b3b906145d003940be0c109f20
Alex Johnson <WebmasterSkeetzo@gmail.com>
2017-08-29 22:22:40 -0700

    1.0.2

--- src/config/index.js
+++ src/config/index.js
@@ -44,9 +44,8 @@ var commandsRegex = "\/[a-z]*",
 config.commandsRegex = new RegExp(commandsRegex, "i");
 config.argumentsRegex = new RegExp(argumentsRegex, "i");
 
-
+keys.call(config);
 logger.call(config);
 crons.call(config);
-keys.call(config);
 
 module.exports = config;
