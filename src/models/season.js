4a4fe7ba763482e8db5c227164ad470272a8ec1b
Alex Johnson <WebmasterSkeetzo@gmail.com>
2017-09-25 17:52:12 -0700

    season

--- src/models/season.js
+++ src/models/season.js
@@ -39,9 +39,9 @@ seasonSchema.pre('save', function(next) {
   if (!this.date.end) {
     this.date.end = moment(this.date.start);
     if (this.date.end.month==0||this.date.end.month>=8)
-      this.date.end.weeks(this.date.end.weeks+16);
+      this.date.end.weeks = this.date.end.weeks+16;
     else if (this.date.end>=4)
-      this.date.end.weeks(this.date.end.weeks+11);
+      this.date.end.weeks = this.date.end.weeks+11;
   }
 
   if (this.isModified('matchups')) 
